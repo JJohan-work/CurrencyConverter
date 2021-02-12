8cb1457aca3e17bdd0ed7b27cfe6f2efcd53e063
Jonah Johansen <johansenjonah+git@gmail.com>
2021-02-12 10:20:29 -0800

    add selection boxes between currencies and functionality to read data that already exists

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,11 +1,11 @@
 export default class CurrencyCall {
   constructor() {
-    this.currencyConv;
+    this.currencyConv = {};
     this.currencyTypes;
     this.nextUpdateTime;
   }
 
-  async makeAPICall(from) {
+  static async makeAPICall(from) {
     try {
       const response  = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${from}`);
       if (!response.ok){
@@ -14,21 +14,6 @@ export default class CurrencyCall {
       return response.json();
     } catch (error) {
       return error;
-    }
-  }
-
-  async checkIfSaved(from) {
-    const callTime = new Date();
-    if (this.nextUpdateTime < callTime && this.nextUpdateTime) {
-      console.log("read from memory");
-    } else {
-      const response = await this.makeAPICall(from);
-      console.log(response);
-      this.currencyConv = response["conversion_rates"];
-      this.nextUpdateTime = response["time_next_update_unix"];
-      console.log(this.currencyConv);
-      console.log(this.nextUpdateTime);
-      return this.currencyConv[from];
     }
   }
 
--- src/main.js
+++ src/main.js
@@ -5,31 +5,46 @@ import './css/styles.css';
 import Currency from './js/currency.js';
 
 
-async function checkIfSaved(from,currencyObject) {
+async function checkIfSaved(from,to,currencyObject) {
   const callTime = new Date();
-  if (currencyObject.nextUpdateTime < callTime && currencyObject.nextUpdateTime) {
+  
+  // currencyObject = storage.conversionRates;
+
+  if (currencyObject.nextUpdateTime < callTime && from in currencyObject.currencyConv) {
     console.log("read from memory");
+    console.log(currencyObject.currencyConv);
+    outputResults(currencyObject.currencyConv[from][to]);
   } else {
-    const response = await currencyObject.makeAPICall(from);
-    console.log(response);
-    currencyObject.currencyConv = response["conversion_rates"];
+    console.log("make new call");
+    const response = await Currency.makeAPICall(from);
+
+    currencyObject.currencyConv[from] = response["conversion_rates"];
+    console.log(currencyObject.currencyConv);
     currencyObject.nextUpdateTime = response["time_next_update_unix"];
-    outputResults(currencyObject.currencyConv[from]);
+    outputResults(currencyObject.currencyConv[from][to]);
+
   }
 }
 
 // function beginAnimation() {}
 
 function outputResults(output) {
-  $("body").html(output)
+  $("#output").html(output);
 }
+
+// function displayError(error) {
+//   $("#error").text(error);
+// }
 
 
 
 function main() {
+  let storage = window.sessionStorage;
   let currencyCall = new Currency;
 
-  checkIfSaved("USD",currencyCall);
+  $("#testButton").on("click", function() {
+    checkIfSaved($("#from").val(),$("#to").val(),currencyCall,storage);
+  });
   
 }
 
