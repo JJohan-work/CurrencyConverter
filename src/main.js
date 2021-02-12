f30ee1c39b1edc8cb95a6f8f2237f9b640145a9e
Jonah Johansen <johansenjonah+git@gmail.com>
2021-02-12 11:10:49 -0800

    update functionality of reading from sessionStorage for multiple currencies

--- src/main.js
+++ src/main.js
@@ -5,25 +5,31 @@ import './css/styles.css';
 import Currency from './js/currency.js';
 
 
-async function checkIfSaved(from,to,currencyObject) {
+async function checkIfSaved(from,to,currencyObject,storage) {
   const callTime = new Date();
-  
-  // currencyObject = storage.conversionRates;
+
+  if (storage[from]) {
+    currencyObject.currencyConv[from] = JSON.parse(storage[from]);
+    currencyObject.nextUpdateTime = JSON.parse(storage.nextUpdateTime);
+  }
 
   if (currencyObject.nextUpdateTime < callTime && from in currencyObject.currencyConv) {
     console.log("read from memory");
-    console.log(currencyObject.currencyConv);
     outputResults(currencyObject.currencyConv[from][to]);
   } else {
     console.log("make new call");
     const response = await Currency.makeAPICall(from);
 
+    storage[from] = JSON.stringify(response["conversion_rates"]);
     currencyObject.currencyConv[from] = response["conversion_rates"];
-    console.log(currencyObject.currencyConv);
+
+    storage.nextUpdateTime = JSON.stringify(response["time_next_update_unix"]);
     currencyObject.nextUpdateTime = response["time_next_update_unix"];
+
     outputResults(currencyObject.currencyConv[from][to]);
 
   }
+  console.log(storage);
 }
 
 // function beginAnimation() {}
